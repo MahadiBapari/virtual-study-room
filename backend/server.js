439ce9223eac61ecf377bb95bc6801fb4e703156
Mahadi Hasan Bapari <hasan5481@gmail.com>
2025-02-03 01:51:33 +0600

    User authentication added

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/users', userRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+// Start server
+const PORT = process.env.PORT || 4200;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
