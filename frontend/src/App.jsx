adbe8a4efc40d6a37342fb3883ae568abe62c68e
Mahadi Hasan Bapari <hasan5481@gmail.com>
2025-02-04 02:21:45 +0600

    Page routing added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Room from "./pages/Room";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/room/:id" element={<Room />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
